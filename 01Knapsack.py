6d7ebace07fe53f1980cec9cb14fb330a51a4ef7
Ramsey Rimkeit <rrimkeit@hotmail.com>
2025-01-25 16:54:47 -0800

    Work in progress on 01Knapsack.py. Going to rename it.

--- 01Knapsack.py
+++ 01Knapsack.py
@@ -1,1 +1,12 @@
 from operator import attrgetter
+
+class Item:
+    def __init__(self, item_weight: int, item_value: int):
+        self.weight = item_weight
+        self.value = item_value
+    
+    
+class Knapsack:
+    def __init__(self, weight: int, items):
+        self.max_weight = weight
+        self.item_list = items        
