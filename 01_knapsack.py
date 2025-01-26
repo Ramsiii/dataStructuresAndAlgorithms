8fec3f1e28cff7f86a187a4d1abcd72f6ee04e8b
Ramsey Rimkeit <rrimkeit@hotmail.com>
2025-01-25 20:21:31 -0800

    Added comments explaining step-by-step what the knapsack_01 function is doing.

--- 01_knapsack.py
+++ 01_knapsack.py
@@ -10,3 +10,41 @@ class Knapsack:
     def __init__(self, weight: int, items):
         self.max_weight = weight
         self.item_list = items        
+
+def knapsack_01(knapsack, item_list):
+    item_list.sort(key = attrgetter('value'), reverse = True)
+    
+    remaining = knapsack.max_weight
+    for item in item_list:
+        if item.weight <= remaining:
+            knapsack.item_list.append(item)
+            remaining = remaining - item.weight
+    
+# main program:
+
+# instances of the Item class with (item_weight, item_value)
+item_1 = Item(6, 25)
+item_2 = Item(8, 42)
+item_3 = Item(12, 60)
+item_4 = Item(18, 95)
+
+# above items stored in a list - item_list is the argument passed to the knapsack_01 function
+item_list = [item_1, item_2, item_3, item_4]
+
+# empty list to be passed as the second argument (items) of the knapsack instance of the Knapsack class 
+initial_knapsack_list = []
+
+# max_weight defined by user input
+max_weight = int(input('Enter the maximum weight the knapsack can hold: '))
+
+# instance of the Knapsack class storing max_weight and empty initial_knapsack_list from above
+knapsack = Knapsack(max_weight, initial_knapsack_list)
+
+# knapsack_01 function being called on the `knapsack` instance which does the following:
+knapsack_01(knapsack, item_list)
+#   `item_list.sort(key = attrgetter(value'), reverse = True)`
+# 1. takes the `item_list`` (starting out as the empty list `initial_knapsack_list`) and 
+#    sorts it in reverse order based on the value which is gotten by the `attrgetter` function from the `operator` module
+#   `remaining = knapsack.max_weight`
+# 2. stores the `max_weight` attribute of `knapsack` (instance of `Knapsack`)  in the `remaining` variable
+# 3. for loop iterates over each item in the `item_list`, which is 
