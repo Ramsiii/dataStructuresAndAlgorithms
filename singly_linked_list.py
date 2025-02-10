4f65b0efe7305dd42a59c678fcde4493ee0bf795
Ramsey Rimkeit <rrimkeit@hotmail.com>
2025-02-10 14:09:41 -0800

    Added a del_tail_node method to the LinkedList class.

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -1,3 +1,4 @@
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -15,14 +16,16 @@ class LinkedList:
         if self.head is None:
             self.head = new_node
             self.tail = new_node
+            new_node.next = None
         
         # if there are one or more nodes. 
         # new_node is set to point at None in the Node class
         else:
-            # point tail node at new_node self.tail.next == <last_node>
+            # point tail node at new_node (befor this action: self.tail.next is None (empty)
             self.tail.next = new_node
             # point tail at new_node
             self.tail = new_node
+            new_node.next = None
             
     def ins_node(self, target_data, new_node):
         current = self.head
@@ -34,6 +37,7 @@ class LinkedList:
                 if current is self.tail:
                     self.tail.next = new_node
                     self.tail = new_node
+                    new_node.next = None
                 else:
                     # Insert between current and next node
                     new_node.next = current.next
@@ -51,6 +55,7 @@ class LinkedList:
         if self.head is None:
             self.head = new_node
             self.tail = new_node
+            new_node.next = None
         
         # if list not empty, insert before head 
         else:
@@ -83,47 +88,122 @@ class LinkedList:
             print(current.data, end=" -> ")
             current = current.next
         print("None")   
-    
+
+
+    def del_head_node(self):
+        # if there is more than one node:
+        if self.head is not None and self.head.next is not None:
+            self.head = self.head.next
+        # if there is only one node:
+        elif self.head is not None and self.head.next is None:
+            self.head = None
+            self.tail = None
+        else:
+            print(f'This linked list is empty.')
+            
+    def del_tail_node(self):
+        
+        if self.head is None:
+            print(f'This linked list is empty.')
+            return
+        
+        # if there is only one node:
+        if self.head is self.tail:
+            self.head = None
+            self.tail = None
+            return
+        # multiple nodes:
+        
+        current = self.head
+
+        while current.next is not self.tail:
+            current = current.next                    
+
+        current.next = None
+        self.tail = current
+        
+        
 if __name__ == '__main__':
     
     l_list = LinkedList()
+    print('Linked List:')
+    l_list.print_list()
     node14 = Node('n')
     node16 = Node('o')
     node17 = Node('p')
     node18 = Node('q')
 
+    print("\nAdd two nodes to empty LL.")
     l_list.add_node(node14)    
+    l_list.add_node(node16)
+
+    print('Linked List:')
+    l_list.print_list()
+    
+    print("\ntest deletion of tail node")
+    l_list.del_tail_node()
+    print('Linked List:')
+    l_list.print_list()
+
+    print("Add node16 'o' back in. Add two more nodes:")
     l_list.add_node(node16)
     l_list.add_node(node17)
     l_list.add_node(node18)    
     
+    print('Linked List:')
+    l_list.print_list()
     node15 = Node('ñ')
     
-    # test insertion of new node after existing node
+    print("\ntest insertion of node15 'ñ' after existing data 'n':")
     l_list.ins_node('n', node15)
     
+    print('Linked List:')
     l_list.print_list()
     
     
     node13 = Node('m')
-    # test insertion of new node as head
+    print("\ntest insertion of new node13 'm' as head")
     l_list.ins_as_head(node13)    
 
+    print('Linked List:')
     l_list.print_list()
     
-    # test deletion of data from LL 
+    print("\ntest deletion of data 'ñ' from LL") 
     l_list.del_node('ñ')
+    print('Linked List:')
     l_list.print_list()
 
-    # test deletion of data not in LL
+    print("\ntest deletion of data 'a' not in LL")
     l_list.del_node('a')
+    print('Linked List:')
     l_list.print_list()
     
-    # test deletion of data at head
+    print("\ntest deletion of data 'm' at head")
     l_list.del_node('m')
+    print('Linked List:')
     l_list.print_list()
     
-    # test deletion of data at tail
+    print("\ntest deletion of data 'q' at tail")
     l_list.del_node('q')
+    print('Linked List:')
     l_list.print_list()
+    
+    print("\ntest deletion of head node")
+    l_list.del_head_node()
+    print('Linked List:')
+    l_list.print_list()
+        
+    
+    # LL2 --------------------
+    
+    print("\nNew instance of LL class LL2.")
+    l_list2 = LinkedList()
+    print("\ntest deletion of head node from empty LL2")
+    l_list2.del_head_node()
 
+    print('\nEmpty Linked List 2:')
+    l_list2.print_list()
+
+    print('Test del_tail_node funciton:')
+    l_list2.del_tail_node()
+    
